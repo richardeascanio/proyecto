554e49e52a11cf50e035c9ab53662a27b1c52d3f
Christian Zarur <christianzarurm@gmail.com>
2018-03-19 14:40:48 -0400

    new apis

---
+++ api/controllers/Seleccion_horarioController.js
@@ -0,0 +1,11 @@
+/**
+ * Seleccion_horarioController
+ *
+ * @description :: Server-side logic for managing seleccion_horarios
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
